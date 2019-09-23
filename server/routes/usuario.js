1a0aa3460420ed0fa043dafda2c07943c12a99d5
Ander Rivero <rivero.ander@gmail.com>
2019-09-23 14:37:50 -0400

    RestSErver Usuario

---
+++ server/routes/usuario.js
@@ -0,0 +1,136 @@
+const Usuario = require('../models/usuario');
+const _ = require('underscore');
+const express = require('express');
+const bcrypt = require('bcrypt');
+const app = express();
+
+//Consultar registro
+app.get('/usuario', (req, res) => {
+
+    let i = Number(req.query.i) || 0;
+    let li = Number(req.query.li) || 5;
+    let filter = { status: true }
+
+    Usuario.find(filter).skip(i).limit(li).exec((err, usuarios) => {
+        if (err) {
+            res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        Usuario.count(filter, (err, conteo) => {
+
+            res.json({
+                ok: true,
+                usuarios,
+                cantidad: conteo
+            })
+        });
+    });
+});
+
+// Crear
+app.post('/usuario', (req, res) => {
+    let body = req.body;
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role
+    });
+
+    usuario.save((err, usuarioDB) => {
+
+        if (err) {
+            res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+
+});
+
+//Actualizar
+app.put('/usuario/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'status']);
+
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+
+        if (err) {
+            res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+
+
+});
+
+
+
+
+app.delete('/usuario/:id', (req, res) => {
+    let id = req.params.id;
+
+    Usuario.findByIdAndUpdate(id, { status: false }, { new: true }, (err, usuarioDB) => {
+
+        if (err) {
+            res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        if (!usuarioDB) {
+            res.status(400).json({
+                ok: false,
+                err: { message: 'Usuario no encontrado' }
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+});
+
+
+/* app.delete('/usuario/:id', (req, res) => {
+    let id = req.params.id;
+
+    Usuario.findByIdAndRemove(id, (err, usuarioDelete) => {
+        if (err) {
+            res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!usuarioDelete) {
+            res.status(400).json({
+                ok: false,
+                err: { message: 'Usuario no encontrado' }
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDelete,
+            mess: 'El usuario fue eliminado'
+        });
+    });
+}); */
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,40 +1,20 @@
 require('./config/config.js');
+
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
-app.get('/usuario', (req, res) => {
-    res.json('getUsuario');
+app.use(require('./routes/usuario.js'));
+
+
+mongoose.connect(process.env.URLDB, (err, res) => {
+    if (err) throw err;
+
+    console.log('Base de datos Online');
 });
-
-app.post('/usuario', (req, res) => {
-
-    if (req.body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mess: 'El usuario es requerido'
-        });
-
-    } else {
-
-        res.json({
-            persona: req.body
-        });
-    }
-});
-
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id;
-    res.json({ id });
-});
-
-app.delete('/usuario', (req, res) => {
-    res.json('delete Usuario');
-});
-
-
 app.listen(process.env.PORT, () => console.log('Escuchando puerto', process.env.PORT));
