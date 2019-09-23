9a7847de8341da31aeaa18b6a1e0babcb20b36db
Ander Rivero <rivero.ander@gmail.com>
2019-09-23 16:10:20 -0400

    prueba

--- server/config/config.js
+++ server/config/config.js
@@ -9,9 +9,8 @@ let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
-} else {
-    urlDB = 'mongodb+srv://cafe-user:Undertaker77@cafe-ykqjg.mongodb.net/test?retryWrites=true&w=majority';
-}
+} else {}
+urlDB = 'mongodb+srv://cafe-user:Undertaker77@cafe-ykqjg.mongodb.net/test?retryWrites=true&w=majority';
 console.log('AQUI AMIGO', process.env.NODE_ENV);
 console.log('AQUI AMIGO', urlDB);
 process.env.URLDB = urlDB;
